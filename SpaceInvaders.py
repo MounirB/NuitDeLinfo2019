278cb42d79e13f68fe524d871675d4c490fe5666
blafabregue <baptistelafabregue@gmail.com>
2019-12-06 00:06:56 +0100

    rename variables

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -33,24 +33,24 @@ def EcranDePresentation():
     if not game.start:
         AffichageScore.configure(text="",font=('Fixedsys',16))
         AffichageVie.configure(text="",font=('Fixedsys',16))
-        can.delete(ALL)
-        fen.after(1500,Titre)
+        canvas.delete(ALL)
+        root.after(1500, Titre)
 
 
 # On afficher le nom du jeu à l'écran
 def Titre():
     global game
     if game.start != 1:
-        can.create_text(320,240,font=('Fixedsys',24),text="SPACE INVADERS",fill='blue')
-        fen.after(2000, Titre2)
+        canvas.create_text(320, 240, font=('Fixedsys', 24), text="SPACE INVADERS", fill='blue')
+        root.after(2000, Titre2)
 
 
 # On affiche le nom de l'auteur ( It's me !! :p )
 def Titre2():
     global game
     if game.start != 1:
-        can.create_text(320,270,font=('Freshbot',18),text="By Shakan972",fill='red')
-        fen.after(3000,LoadMeilleurScore)
+        canvas.create_text(320, 270, font=('Freshbot', 18), text="By Shakan972", fill='red')
+        root.after(3000, LoadMeilleurScore)
 
 
 # Cette fonction va permettre d'enregistrer
@@ -67,9 +67,9 @@ def SaveMeilleurScore(resultat):
         FichierScore=open('HighScore','w')
         pickle.dump(resultat,FichierScore)
         FichierScore.close()
-        fen.after(2000,MessageRecord)
+        root.after(2000, MessageRecord)
     else:
-        fen.after(15000,EcranDePresentation)
+        root.after(15000, EcranDePresentation)
     FichierScore.close()
 
 
@@ -77,9 +77,9 @@ def SaveMeilleurScore(resultat):
 # lui indiquant qu'il a établit un
 # nouveau record :D
 def MessageRecord():
-    can.delete(ALL)
-    can.create_text(320,240,font=('Georgia',18),text="Vous avez établi un nouveau record !!",fill='red')
-    fen.after(3000,LoadMeilleurScore)
+    canvas.delete(ALL)
+    canvas.create_text(320, 240, font=('Georgia', 18), text="Vous avez établi un nouveau record !!", fill='red')
+    root.after(3000, LoadMeilleurScore)
 
 
 # Quant à cette fonction elle va permettre
@@ -90,11 +90,11 @@ def LoadMeilleurScore():
         # FichierScore=open('HighScore','r')
         # lecture=pickle.load(FichierScore)
         lecture = "WIN"
-        can.delete(ALL)
-        can.create_text(320,240,font=('Fixedsys',24),text="HIGH SCORE",fill='blue')
-        can.create_text(320,270,font=('Fixedsys',24),text=str(lecture),fill='blue')
+        canvas.delete(ALL)
+        canvas.create_text(320, 240, font=('Fixedsys', 24), text="HIGH SCORE", fill='blue')
+        canvas.create_text(320, 270, font=('Fixedsys', 24), text=str(lecture), fill='blue')
         FichierScore.close()
-        fen.after(3000,EcranDePresentation)
+        root.after(3000, EcranDePresentation)
 
 
 # Cette fonction permet de vérifier
@@ -120,10 +120,10 @@ def new_game():
 # de la destruction d'un ennemi
 def score(donnee, x, y, x2, y2):
     global afficherScore
-    afficherScore.append(can.create_text(x+x2, y+y2,
-                        font=('Fixedsys', 8),
-                        text=str(donnee)+' pts',fill='red'))
-    fen.after(1500,EffacerScore)
+    afficherScore.append(canvas.create_text(x + x2, y + y2,
+                                            font=('Fixedsys', 8),
+                                            text=str(donnee)+' pts', fill='red'))
+    root.after(1500, EffacerScore)
 
 
 # Cette fonction permet d'effacer
@@ -133,7 +133,7 @@ def EffacerScore():
     global afficherScore
     i=0
     while i<len(afficherScore):
-        can.delete(afficherScore[i])
+        canvas.delete(afficherScore[i])
         i+=1
 
 # La fonction ci-dessous permet
@@ -163,23 +163,22 @@ def move(dx):
 # Cette fonction gère le tir des ennemis
 # et vérifie si un a atteint le canon
 # mobile du joueur
-def launch_enemy_missil():
+def launch_enemy_missile():
     global game
-    if not game.paused:
-        if game.start:
-            # Choose enemy that fires missil
-            enemy = game.enemies[random.randint(0, len(game.enemies))]
-            game.enemies_missil.append(EnemyMissil(enemy.x, enemy.y))
-            animate_enemies_missil()
-    fen.after(100, launch_enemy_missil)
+    if not game.paused and  game.start and game.enemies:
+        # Choose enemy that fires missil
+        enemy = game.enemies[random.randint(0, len(game.enemies)-1)]
+        game.enemies_missile.append(EnemyMissile(enemy.x, enemy.y))
+        animate_enemies_missile()
+    root.after(1000, launch_enemy_missile)
         
 
 # Cette fonction permet d'animer l'obus tiré
 # par un ennemi
-def animate_enemies_missil():
+def animate_enemies_missile():
     global game
     if not game.paused:
-        for missil in game.enemies_missil:
+        for missil in game.enemies_missile:
             missil.move()
 
             # Si un tir ennemi parvient à son objectif en
@@ -195,29 +194,29 @@ def animate_enemies_missil():
                 
                 if game.student.lives >=1:
                     AffichageVie.configure(text="Lives : "+str(game.student.lives), font=('Fixedsys',16))
-                    fen.after(500, game.student.revive())
+                    root.after(500, game.student.revive())
                 else:
 
                     # On efface l'écran
-                    can.delete(ALL)
+                    canvas.delete(ALL)
                     image()
-                    can.create_text(320,240,font=('Fixedsys',18),text="Game Over !!", fill='red')
+                    canvas.create_text(320, 240, font=('Fixedsys', 18), text="Game Over !!", fill='red')
                     game.stop_animation()
                     game.start = False
 
                     # On vérifie le score
                     SaveMeilleurScore(game.score)
 
-        fen.after(50, animate_enemies_missil)
+        root.after(50, animate_enemies_missile)
+
 
 # Cette fonction va permettre d'afficher un
 # paysage post-apocalyptique si le joueur
 # fait un game over !! :( :(
-
 def image():
     global photo
     photo=PhotoImage(file='apocalypse.gif')
-    can.create_image(320,240,image=photo)
+    canvas.create_image(320, 240, image=photo)
     
 
 # Cette fonction permet de ressuciter
@@ -232,30 +231,30 @@ def res_student():
 # Cette fonction va permettre de gérer le tir du canon
 # ainsi que les collisions avec les cibles situées en
 # haut du canevas :)
-def launch_missil(event):
+def launch_missile(event):
     global game
     if game.start:
         if not game.paused:
-            game.missil = Missil(game.student.x, game.student.y-20)
-            if game.missil.fired:
+            game.missile = Missile(game.student.x, game.student.y - 20)
+            if game.missile.fired:
                 time.sleep(0.09)
-                animate_missil()
+                animate_missile()
 
 
 # Cette fonction va permettre d'animer l'obus tiré par
 # le canon mobile
-def animate_missil():
+def animate_missile():
     global game
 
-    if not game.paused and (game.missil is not None) and (not game.stop_animations):
-        if game.missil.fired:
-            game.missil.move()
+    if not game.paused and (game.missile is not None) and (not game.stop_animations):
+        if game.missile.fired:
+            game.missile.move()
 
             for enemy in game.enemies:
-                if game.missil.x+5 >= enemy.x and game.missil.x-5 <= enemy.x+60:
-                    if game.missil.y+5 >= enemy.y and game.missil.y-5 <= enemy.y+60:
+                if game.missile.x+5 >= enemy.x and game.missile.x-5 <= enemy.x+60:
+                    if game.missile.y+5 >= enemy.y and game.missile.y-5 <= enemy.y+60:
                         game.score += 50
-                        game.missil.explod()
+                        game.missile.explod()
                         AffichageScore.configure(text="Score : "+str(game.score), font=('Fixedsys',16))
                         # score(50, enemy.x, enemy.y, 30, 20)
                         enemy.explod()
@@ -265,7 +264,7 @@ def animate_missil():
             if not game.enemies:
                game.init_board()
             else:
-                fen.after(50, animate_missil)
+                root.after(50, animate_missile)
     
 
 # Les deux fonctions ci-dessous permettent
@@ -303,22 +302,22 @@ class Student:
     def __init__(self):
         self.x = X_LIMIT[0] + X_LIMIT[1]//2
         self.y = Y_LIMIT[1] - 5
-        self.body = can.create_rectangle(self.x, self.y, self.x + 20, self.y + 20, fill='green')
+        self.body = canvas.create_rectangle(self.x, self.y, self.x + 20, self.y + 20, fill='green')
         self.lives = 3
 
     def explod(self):
-        can.delete(self.body)
+        canvas.delete(self.body)
 
     def revive(self):
         self.x = X_LIMIT[0] + X_LIMIT[1]//2
         self.y = Y_LIMIT[1] - 5
-        can.delete(self.body)
-        self.body = can.create_rectangle(self.x, self.y, self.x + 20, self.y + 20, fill='green')
+        canvas.delete(self.body)
+        self.body = canvas.create_rectangle(self.x, self.y, self.x + 20, self.y + 20, fill='green')
         self.lives -= 1
 
     def redraw(self):
-        can.delete(self.body)
-        self.body = can.create_rectangle(self.x, self.y, self.x + 20, self.y + 20, fill='green')
+        canvas.delete(self.body)
+        self.body = canvas.create_rectangle(self.x, self.y, self.x + 20, self.y + 20, fill='green')
 
 
 class Enemy:
@@ -329,36 +328,36 @@ class Enemy:
         self.x = random.randint(X_LIMIT[0], X_LIMIT[1])
         self.y = random.randint(Y_LIMIT[0], Y_LIMIT[1]//4)
         self.body = []
-        self.body.append(can.create_rectangle(self.x, self.y, self.x + 60, self.y + 20, fill='blue'))
-        self.body.append(can.create_rectangle(self.x, self.y, self.x + 20, self.y + 40, fill='blue'))
-        self.body.append(can.create_rectangle(self.x + 40, self.y, self.x + 60, self.y + 40, fill='blue'))
+        self.body.append(canvas.create_rectangle(self.x, self.y, self.x + 60, self.y + 20, fill='blue'))
+        self.body.append(canvas.create_rectangle(self.x, self.y, self.x + 20, self.y + 40, fill='blue'))
+        self.body.append(canvas.create_rectangle(self.x + 40, self.y, self.x + 60, self.y + 40, fill='blue'))
 
     def explod(self):
         for b in self.body:
-            can.delete(b)
+            canvas.delete(b)
 
     def redraw(self):
         for b in self.body:
-            can.delete(b)
+            canvas.delete(b)
         self.body = []
-        self.body.append(can.create_rectangle(self.x, self.y, self.x + 60, self.y + 20, fill='blue'))
-        self.body.append(can.create_rectangle(self.x, self.y, self.x + 20, self.y + 40, fill='blue'))
-        self.body.append(can.create_rectangle(self.x + 40, self.y, self.x + 60, self.y + 40, fill='blue'))
+        self.body.append(canvas.create_rectangle(self.x, self.y, self.x + 60, self.y + 20, fill='blue'))
+        self.body.append(canvas.create_rectangle(self.x, self.y, self.x + 20, self.y + 40, fill='blue'))
+        self.body.append(canvas.create_rectangle(self.x + 40, self.y, self.x + 60, self.y + 40, fill='blue'))
 
 
-class Missil:
+class Missile:
     """
     """
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.body = can.create_oval(self.x, self.y, self.x+20, self.y+20,fill='yellow')
+        self.body = canvas.create_oval(self.x, self.y, self.x + 20, self.y + 20, fill='yellow')
         self.fired = True
-        self.dy = 10
+        self.dy = MISSILE_DY
 
     def explod(self):
         self.fired = 0
-        can.delete(self.body)
+        canvas.delete(self.body)
 
     def move(self):
         self.y -= self.dy
@@ -368,23 +367,24 @@ class Missil:
             self.redraw()
 
     def redraw(self):
-        can.delete(self.body)
-        self.body = can.create_oval(self.x, self.y, self.x+20, self.y+20,fill='yellow')
+        canvas.delete(self.body)
+        self.body = canvas.create_oval(self.x, self.y, self.x + 20, self.y + 20, fill='yellow')
 
 
-class EnemyMissil:
+class EnemyMissile:
     """
     """
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.body = can.create_oval(self.x, self.y, self.x+4, self.y+20,fill='orange')
-        self.fired = True
-        self.dy = 5
+        self.body = canvas.create_oval(self.x, self.y, self.x + 4, self.y + 20, fill='orange')
+        self.dy = ENEMIES_MISSILE_DY
 
     def explod(self):
+        global game
         self.fired = 0
-        can.delete(self.body)
+        canvas.delete(self.body)
+        game.enemies_missile.remove(self)
 
     def move(self):
         self.y += self.dy
@@ -394,8 +394,8 @@ class EnemyMissil:
             self.redraw()
 
     def redraw(self):
-        can.delete(self.body)
-        self.body = can.create_oval(self.x, self.y, self.x+4, self.y+20,fill='orange')
+        canvas.delete(self.body)
+        self.body = canvas.create_oval(self.x, self.y, self.x + 4, self.y + 20, fill='orange')
 
 
 class Game:
@@ -407,14 +407,14 @@ class Game:
         self.stop_animations = False
         self.score = 0
         self.student = Student()
-        self.missil = None
+        self.missile = None
         self.enemies = []
-        self.enemies_missil = []
+        self.enemies_missile = []
         self.paused = False
         self.paused_body = None
 
     def init_board(self):
-        can.delete(ALL)
+        canvas.delete(ALL)
         self.score = 0
         self.student = Student()
         self.enemies = []
@@ -423,33 +423,32 @@ class Game:
 
         # On efface tout à l'écran
         background = PhotoImage(file='earth.gif')
-        can.create_image(240, 320, image=background)
+        canvas.create_image(240, 320, image=background)
 
         AffichageScore.configure(text="Score : " + str(self.score))
         AffichageVie.configure(text="Vies : " + str(self.student.lives))
 
         self.start = True
         self.paused = False
-        fen.after(200, launch_enemy_missil())
+        root.after(200, launch_enemy_missile())
 
     def stop_animation(self):
-        self.stop_animation = True
+        self.stop_animations = True
 
     def launch_animation(self):
         self.stop_animations = False
-        animate_missil()
-        animate_enemies_missil()
+        animate_missile()
+        animate_enemies_missile()
 
     def pause_pressed(self):
         if self.paused:
             self.paused = False
-            can.delete(self.paused_body)
+            canvas.delete(self.paused_body)
             self.launch_animation()
         else:
             self.paused = True
-            self.paused_body = can.create_text(320, 240, font=('Fixedsys', 18), text="PAUSE")
+            self.paused_body = canvas.create_text(320, 240, font=('Fixedsys', 18), text="PAUSE")
             self.stop_animation()
-
 
 
 #######################
@@ -458,7 +457,6 @@ class Game:
 #                     #
 #######################
 if __name__ == "__main__":
-
     NB_START_ENEMIES = 4
     NB_LIVES_START = 3
     WIDTH = 480
@@ -466,28 +464,30 @@ if __name__ == "__main__":
     MARGIN = 5
     X_LIMIT = [MARGIN, WIDTH-MARGIN]
     Y_LIMIT = [MARGIN, HEIGHT-MARGIN]
+    ENEMIES_MISSILE_DY = 3
+    MISSILE_DY = 10
 
     # Création de la fenêtre principale
 
-    fen=Tk()
+    root = Tk()
 
     # Titre de la fenêtre
 
-    fen.title('University Invaders')
+    root.title('University Invaders')
 
     # Définition du canevas ( Ecran de jeu )
 
-    can = Canvas(fen, width=WIDTH, height=HEIGHT, bg='black')
+    canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg='black')
 
     # Définition des touches qui vont permettre
     # de diriger le canon mobile
 
-    can.bind_all("<Right>", right)
-    can.bind_all("<Left>", left)
-    can.bind_all("<space>", launch_missil)
-    can.bind_all("<p>", pause)
+    canvas.bind_all("<Right>", right)
+    canvas.bind_all("<Left>", left)
+    canvas.bind_all("<space>", launch_missile)
+    canvas.bind_all("<p>", pause)
 
-    can.grid(row=1, column=0, columnspan=2, rowspan=3)
+    canvas.grid(row=1, column=0, columnspan=2, rowspan=3)
 
     # Installation d'une image de fond
     # pour être plus dans l'ambiance 8)
@@ -497,15 +497,15 @@ if __name__ == "__main__":
 
     # Ce bouton permet de commencer une nouvelle partie
 
-    Button(fen, text="Nouvelle partie", command=new_game).grid(row=2, column=2, sticky=N, padx=5)
-    Button(fen, text="Quitter", command=fen.destroy).grid(row=3, column=2, sticky=N, padx=5)
+    Button(root, text="Nouvelle partie", command=new_game).grid(row=2, column=2, sticky=N, padx=5)
+    Button(root, text="Quitter", command=root.destroy).grid(row=3, column=2, sticky=N, padx=5)
 
 
     # On affiche les indications concernant
     # le score et les vies restantes du joueur
 
-    AffichageScore=Label(fen,font=('Fixedsys',16))
-    AffichageVie=Label(fen,font=('Fixedsys',16))
+    AffichageScore=Label(root, font=('Fixedsys', 16))
+    AffichageVie=Label(root, font=('Fixedsys', 16))
     AffichageScore.grid(row=0,column=0,sticky=W)
     AffichageVie.grid(row=0,column=1,sticky=E)
 
@@ -528,5 +528,5 @@ if __name__ == "__main__":
 
     # On met le gestionnaire d'événements en route
 
-    fen.mainloop()
+    root.mainloop()
 
